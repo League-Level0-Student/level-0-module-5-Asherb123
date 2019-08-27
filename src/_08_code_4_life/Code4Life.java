54ff595877fb0ecabcc42d87f792e89cb44e2689
League Student <league@iMac-15.attlocal.net>
2019-08-26 18:27:40 -0700

    I did lots of stuff :sunglasses:

--- src/_08_code_4_life/Code4Life.java
+++ src/_08_code_4_life/Code4Life.java
@@ -11,20 +11,31 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
+public static void main(String[] args) {
+	
 
 	// 1. Ask the user how many hours they spent coding this week.
-
+	String s=JOptionPane.showInputDialog("how manyee awires have you spent coding this week?");
+	int a =Integer.parseInt(s);
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-
+	if (a<2) {
+		JOptionPane.showMessageDialog(null, "You a noob");
+	}
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
-
+	if (a>=3&&a<=5) {
+		JOptionPane.showMessageDialog(null, "you bing bong");
+	}
 	// 4. If it is more than 5, call the method below to play the Batman theme song.
-
-	private static void playBatmanTheme() {
+	if (a>5) {
+		JOptionPane.showMessageDialog(null, "Holy mother pooper");
+	playBatmanTheme();
+	}
+}	private static void playBatmanTheme() {
 		try {
 			AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
 			sound.play();
@@ -35,3 +46,5 @@ public class Code4Life {
 	}
 
 }
+
+
