944d31d3a344293a127f11881d166bf68bfe98f4
League Student <league@iMac-15.attlocal.net>
2019-09-09 18:26:32 -0700

    almost done with level 0

---
+++ src/_06_for_loop_gauntlet/ForLoopGountlet.java
@@ -0,0 +1,85 @@
+package _06_for_loop_gauntlet;
+
+import javax.swing.JOptionPane;
+
+public class ForLoopGountlet {
+public static void main(String[] args) {
+	
+
+
+ for (int i = 0; i < 100; i++) {
+	System.out.println(i);
+}
+
+for (int i = 100; i > 0; i--) {
+	System.out.println(i);
+}
+for (int i = 0; i < 100; i++) {
+	if (i%2==0) {
+	System.out.println(i);	
+	}
+}
+
+for (int i = 0; i <99; i++) {
+	if (i%2==1) {
+		System.out.println(i);
+	}
+}
+for (int i = 1; i < 500; i++) {
+	if (i%2==0) {
+		JOptionPane.showMessageDialog(null, "Even number");
+		
+			JOptionPane.showMessageDialog(null, "Number is odd");
+		}
+	if (condition) {
+		
+	}
+	System.out.println(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
